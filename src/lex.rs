42e0b3be09e80974e9fd7e5ffdd747277ee2297a
misebox <misebox@gmail.com>
2018-10-14 20:17:43 +0900

    Add tests for lexer

--- src/lex.rs
+++ src/lex.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 pub enum TokenType {
     Doctype(String),
     NewLine,
@@ -32,7 +32,7 @@ impl fmt::Display for TokenType {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 pub struct Token {
     ty: TokenType,
     start: usize,
@@ -339,31 +339,233 @@ mod tests {
         assert_eq!(tokens.len(), 1);
         tokens[0].clone()
     }
+    fn tokenize(src: &str) -> Vec<Token> {
+        let mut lex = Lexer::new(src.to_string());
+        lex.tokenize();
+        lex.get_tokens()
+    }
 
     #[test]
     fn lexer_works_html() {
         let src = "html";
         let token = tokenize_one(src);
-        let _name = "html".to_string();
-        assert!(if let TokenType::Tag(_name) = token.ty {
-            true
-        } else {
-            false
-        });
-        assert_eq!(token.start, 0);
-        assert_eq!(token.end, 4);
+        assert_eq![
+            token,
+            Token {
+                ty: TokenType::Tag("html".to_string()),
+                start: 0,
+                end: 4,
+            }
+        ];
     }
     #[test]
     fn lexer_works_doctype() {
         let src = "doctype html";
         let token = tokenize_one(src);
-        let _name = "html".to_string();
-        assert!(if let TokenType::Doctype(_name) = token.ty {
-            true
-        } else {
-            false
-        });
-        assert_eq!(token.start, 0);
-        assert_eq!(token.end, 12);
+        assert_eq![
+            token,
+            Token {
+                ty: TokenType::Doctype("html".to_string()),
+                start: 0,
+                end: 12,
+            }
+        ];
+    }
+    #[test]
+    fn lexer_works_id() {
+        let src = "#abc";
+        let token = tokenize_one(src);
+        assert_eq![
+            token,
+            Token {
+                ty: TokenType::Id("abc".to_string()),
+                start: 0,
+                end: 4,
+            }
+        ];
+    }
+    #[test]
+    fn lexer_works_class() {
+        let src = ".class-name1.class-name2";
+        let tokens = tokenize(src);
+        assert_eq![
+            tokens[0],
+            Token {
+                ty: TokenType::Class("class-name1".to_string()),
+                start: 0,
+                end: 12,
+            }
+        ];
+        assert_eq![
+            tokens[1],
+            Token {
+                ty: TokenType::Class("class-name2".to_string()),
+                start: 12,
+                end: 24,
+            }
+        ];
+    }
+    #[test]
+    fn lexer_works_attr() {
+        let src = r#"(aa=AA bb="B B" cc="'CC'")"#;
+        let tokens = tokenize(src);
+        assert_eq![
+            tokens[0],
+            Token {
+                ty: TokenType::Attr("aa".to_string(), "AA".to_string()),
+                start: 1,
+                end: 6,
+            }
+        ];
+        assert_eq![
+            tokens[1],
+            Token {
+                ty: TokenType::Attr("bb".to_string(), "B B".to_string()),
+                start: 7,
+                end: 15,
+            }
+        ];
+        assert_eq![
+            tokens[2],
+            Token {
+                ty: TokenType::Attr("cc".to_string(), "'CC'".to_string()),
+                start: 16,
+                end: 25,
+            }
+        ];
+    }
+    #[test]
+    fn lexer_works_colon() {
+        let src = r#"div: span: img"#;
+        let tokens = tokenize(src);
+        assert_eq![
+            tokens[0],
+            Token {
+                ty: TokenType::Tag("div".to_string()),
+                start: 0,
+                end: 3,
+            }
+        ];
+        assert_eq![
+            tokens[1],
+            Token {
+                ty: TokenType::Colon,
+                start: 3,
+                end: 4,
+            }
+        ];
+        assert_eq![
+            tokens[2],
+            Token {
+                ty: TokenType::Tag("span".to_string()),
+                start: 5,
+                end: 9,
+            }
+        ];
+        assert_eq![
+            tokens[3],
+            Token {
+                ty: TokenType::Colon,
+                start: 9,
+                end: 10,
+            }
+        ];
+        assert_eq![
+            tokens[4],
+            Token {
+                ty: TokenType::Tag("img".to_string()),
+                start: 11,
+                end: 14,
+            }
+        ];
+    }
+    #[test]
+    fn lexer_works_all_in_one() {
+        let src = r##"doctype html
+html
+  head
+    meta(charset="UTF-8")
+    title ページタイトル
+  body
+    .wrapper
+      #header
+        .menu
+          a(href="#" alt="link"): img
+      #container
+        ul.item-list
+          li.item text1
+          li.item text2
+          li.item text3
+"##;
+        let mut lex = Lexer::new(src.to_string());
+        lex.tokenize();
+        let tokens = lex.get_tokens();
+        assert_eq!(tokens.len(), 58);
+        let expects = vec![
+            TokenType::Doctype("html".to_string()),
+            TokenType::Tag("html".to_string()),
+            TokenType::NewLine,
+            TokenType::Indent,
+            TokenType::Tag("head".to_string()),
+            TokenType::NewLine,
+            TokenType::Indent,
+            TokenType::Tag("meta".to_string()),
+            TokenType::Attr("charset".to_string(), "UTF-8".to_string()),
+            TokenType::NewLine,
+            TokenType::Tag("title".to_string()),
+            TokenType::Text("ページタイトル".to_string()),
+            TokenType::NewLine,
+            TokenType::Outdent,
+            TokenType::Tag("body".to_string()),
+            TokenType::NewLine,
+            TokenType::Indent,
+            TokenType::Class("wrapper".to_string()),
+            TokenType::NewLine,
+            TokenType::Indent,
+            TokenType::Id("header".to_string()),
+            TokenType::NewLine,
+            TokenType::Indent,
+            TokenType::Class("menu".to_string()),
+            TokenType::NewLine,
+            TokenType::Indent,
+            TokenType::Tag("a".to_string()),
+            TokenType::Attr("href".to_string(), "#".to_string()),
+            TokenType::Attr("alt".to_string(), "link".to_string()),
+            TokenType::Colon,
+            TokenType::Tag("img".to_string()),
+            TokenType::NewLine,
+            TokenType::Outdent,
+            TokenType::Outdent,
+            TokenType::Id("container".to_string()),
+            TokenType::NewLine,
+            TokenType::Indent,
+            TokenType::Tag("ul".to_string()),
+            TokenType::Class("item-list".to_string()),
+            TokenType::NewLine,
+            TokenType::Indent,
+            TokenType::Tag("li".to_string()),
+            TokenType::Class("item".to_string()),
+            TokenType::Text("text1".to_string()),
+            TokenType::NewLine,
+            TokenType::Tag("li".to_string()),
+            TokenType::Class("item".to_string()),
+            TokenType::Text("text2".to_string()),
+            TokenType::NewLine,
+            TokenType::Tag("li".to_string()),
+            TokenType::Class("item".to_string()),
+            TokenType::Text("text3".to_string()),
+            TokenType::NewLine,
+            TokenType::Outdent,
+            TokenType::Outdent,
+            TokenType::Outdent,
+            TokenType::Outdent,
+            TokenType::Outdent,
+        ];
+        let zipped = tokens.iter().zip(expects.iter());
+        for (actual, expect) in zipped {
+            println!("{}", actual.ty);
+            println!("{}", expect);
+            assert!(*actual.get_type() == *expect);
+        }
     }
 }
